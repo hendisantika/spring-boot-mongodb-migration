c332d167bb5c4f9ee5e6c8d54bbd58d7faf4b704
hendisantika <hendisantika@yahoo.co.id>
2024-12-12 12:33:41 +0700

    Add getAllDepartments in DepartmentController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongodbmigration/controller/DepartmentController.java
+++ src/main/java/id/my/hendisantika/mongodbmigration/controller/DepartmentController.java
@@ -1,10 +1,20 @@
 package id.my.hendisantika.mongodbmigration.controller;
 
+import id.my.hendisantika.mongodbmigration.domain.Department;
 import id.my.hendisantika.mongodbmigration.repository.DepartmentRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +32,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Department", description = "Department CRUD API with documentation annotations")
 public class DepartmentController {
     private final DepartmentRepository departmentRepository;
+
+    @GetMapping()
+    @Operation(summary = "Get All Cars")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    public ResponseEntity<List<Department>> getAllDepartments() {
+        return new ResponseEntity<>(departmentRepository.findAll(), HttpStatus.OK);
+    }
 }
