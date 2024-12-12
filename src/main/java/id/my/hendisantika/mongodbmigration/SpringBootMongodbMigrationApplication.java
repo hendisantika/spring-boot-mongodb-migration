43ecba825f0830c13543342556e668bac2de6fd9
hendisantika <hendisantika@yahoo.co.id>
2024-12-12 12:07:01 +0700

    Add @EnableMongock🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongodbmigration/SpringBootMongodbMigrationApplication.java
+++ src/main/java/id/my/hendisantika/mongodbmigration/SpringBootMongodbMigrationApplication.java
@@ -1,9 +1,11 @@
 package id.my.hendisantika.mongodbmigration;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongock
 public class SpringBootMongodbMigrationApplication {
 
     public static void main(String[] args) {
