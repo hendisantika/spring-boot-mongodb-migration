98f7d65a9389d8f2c26a61e4973caccced1ca006
hendisantika <hendisantika@yahoo.co.id>
2024-12-12 12:42:40 +0700

    Add updateEmployee in EmployeeController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongodbmigration/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/mongodbmigration/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,5 +65,19 @@ public class EmployeeController {
     })
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") String id) {
         return new ResponseEntity<>(employeeRepository.findById(id).get(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update Employee by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") String id, @RequestBody Employee employee) {
+        if (employeeRepository.existsById(id)) {
+            return new ResponseEntity<>(employeeRepository.save(employee), HttpStatus.ACCEPTED);
+        }
+
+        throw new IllegalArgumentException("Car with id " + id + "not found");
     }
 }
