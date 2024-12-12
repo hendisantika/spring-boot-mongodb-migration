7b87ec7acfe53740fb34dcf0c2c598ce8181b048
hendisantika <hendisantika@yahoo.co.id>
2024-12-12 12:27:21 +0700

    Add Spring Doc API Configs🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongodbmigration/controller/DepartmentController.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.mongodbmigration.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mongodb-migration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/12/24
+ * Time: 12.21
+ * To change this template use File | Settings | File Templates.
+ */
+public class DepartmentController {
+}
