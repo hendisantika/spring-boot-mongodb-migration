a7dbcbc7ab91c6ad2d6c4bf04ff7eb700a5923f4
hendisantika <hendisantika@yahoo.co.id>
2024-12-12 12:36:40 +0700

    Add updateDepartment in DepartmentController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/mongodbmigration/controller/DepartmentController.java
+++ src/main/java/id/my/hendisantika/mongodbmigration/controller/DepartmentController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,5 +65,19 @@ public class DepartmentController {
     })
     public ResponseEntity<Department> getDepartmentById(@PathVariable("id") String id) {
         return new ResponseEntity<>(departmentRepository.findById(id).get(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update Department by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    public ResponseEntity<Department> updateDepartment(@PathVariable("id") String id, @RequestBody Department department) {
+        if (departmentRepository.existsById(id)) {
+            return new ResponseEntity<>(departmentRepository.save(department), HttpStatus.ACCEPTED);
+        }
+
+        throw new IllegalArgumentException("Car with id " + id + "not found");
     }
 }
