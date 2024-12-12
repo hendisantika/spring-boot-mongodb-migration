c83caa4d0b6a3fc0a66a2e8045b18d1eb3f164e9
hendisantika <hendisantika@yahoo.co.id>
2024-12-12 12:00:32 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/mongodbmigration/SpringBootMongodbMigrationApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.mongodbmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongodbMigrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/mongodbmigration/SpringBootMongodbMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.mongodbmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbMigrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
